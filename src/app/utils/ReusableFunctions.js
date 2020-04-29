f9961e22ff51f6c33450641be29a6b3e4b54715f
nirajrajgor <nirajr1992@gmail.com>
2020-04-29 16:38:26 +0530

    Format date time & debounce function added.

--- src/app/utils/ReusableFunctions.js
+++ src/app/utils/ReusableFunctions.js
@@ -1,1 +1,18 @@
 export const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max));
+
+// as long as it continues to be invoked, it will not be triggered
+export const debounce = (func, delay) => {
+	let inDebounce
+	return function () {
+		const context = this
+		const args = arguments
+		clearTimeout(inDebounce)
+		inDebounce = setTimeout(() => func.apply(context, args), delay)
+	}
+}
+
+export const formatDateTime = (unixTime) => {
+	// Create a new JavaScript Date object based on the timestamp
+	// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+	return new Date(unixTime * 1000).toLocaleString('en-IN', { month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' })
+}
