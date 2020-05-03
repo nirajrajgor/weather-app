f9c7d3fe1728cd2f3a48423933e40a875dbc7ded
nirajrajgor <nirajr1992@gmail.com>
2020-05-03 19:27:04 +0530

    Updates

---
+++ precache-manifest.d77bfbce911f9f0dde7716cf09654f9d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b25797d3f25c87dd5ce5f795f039724",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ed7e3806bd85eb3a8064",
+    "url": "/weather-app/static/css/main.60c7ad71.chunk.css"
+  },
+  {
+    "revision": "7744c5dfd6bf7df0afee",
+    "url": "/weather-app/static/js/2.d15ddb94.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/weather-app/static/js/2.d15ddb94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7e3806bd85eb3a8064",
+    "url": "/weather-app/static/js/main.d6adbbab.chunk.js"
+  },
+  {
+    "revision": "ea6a87d0551586d0c7b5",
+    "url": "/weather-app/static/js/runtime-main.34dd0968.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/weather-app/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/weather-app/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/weather-app/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/weather-app/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "3b3dfec5b63341b46b1b013558912b78",
+    "url": "/weather-app/static/media/mumbai.3b3dfec5.jpg"
+  }
+]);
